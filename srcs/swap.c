3420e43f76bcbe224ca787e50d3ee3a39984b503
aleksiel HORAK <ahorak@c2r4s1.wethinkcode.co.za>
2018-08-27 11:50:13 +0200

    Fixed it so ss will still run sa if list_b is empty

--- srcs/swap.c
+++ srcs/swap.c
@@ -50,9 +50,8 @@ void	ss(t_list *list_a, t_list *list_b)
 	t_node	*node_a;
 	t_node	*node_b;
 
-	if ((list_a != NULL && list_a->head != NULL && \
-	list_a->head->next != NULL) && (list_b != NULL && \
-	list_b->head != NULL && list_b->head->next != NULL))
+	if (list_a != NULL && list_a->head != NULL && \
+	list_a->head->next != NULL)
 	{
 		node_a = list_a->head;
 		list_a->head = node_a->next;
@@ -60,13 +59,15 @@ void	ss(t_list *list_a, t_list *list_b)
 		node_a->prev = list_a->head;
 		node_a->next = list_a->head->next;
 		list_a->head->next = node_a;
-	//	node_a->next->prev = node_a;
+	}
+	if (list_b != NULL && list_b->head != NULL && \
+	list_b->head->next != NULL)
+	{
 		node_b = list_b->head;
 		list_b->head = node_b->next;
 		list_b->head->prev = NULL;
 		node_b->prev = list_b->head;
 		node_b->next = list_b->head->next;
 		list_b->head->next = node_b;
-	//	node_b->next->prev = node_b;
 	}
 }
