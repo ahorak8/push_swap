a4eb7f97749a8e2434b073cbdcb7e5c8ce2edd43
aleksiel HORAK <ahorak@c2r4s1.wethinkcode.co.za>
2018-09-05 17:02:02 +0200

    swap makes allowances for the tail node

--- srcs/swap.c
+++ srcs/swap.c
@@ -15,6 +15,7 @@
 void	sa(t_list *list_a)
 {
 	t_node	*node;
+	t_node	*current;
 
 	if (list_a != NULL && list_a->head != NULL && list_a->head->next != NULL)
 	{
@@ -24,12 +25,21 @@ void	sa(t_list *list_a)
 		node->prev = list_a->head;
 		node->next = list_a->head->next;
 		list_a->head->next = node;
+
+		current = list_a->head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+			if (current->next == NULL)
+				list_a->tail = current;
+		}
 	}
 }
 
 void	sb(t_list *list_b)
 {
 	t_node	*node;
+	t_node	*current;
 
 	if (list_b != NULL && list_b->head != NULL && \
 	list_b->head->next != NULL)
@@ -40,6 +50,14 @@ void	sb(t_list *list_b)
 		node->prev = list_b->head;
 		node->next = list_b->head->next;
 		list_b->head->next = node;
+
+		current = list_b->head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+			if (current->next == NULL)
+				list_b->tail = current;
+		}
 	}
 }
 
@@ -47,6 +65,8 @@ void	ss(t_list *list_a, t_list *list_b)
 {
 	t_node	*node_a;
 	t_node	*node_b;
+	t_node	*current_a;
+	t_node	*current_b;
 
 	if (list_a != NULL && list_a->head != NULL && \
 	list_a->head->next != NULL)
@@ -57,6 +77,14 @@ void	ss(t_list *list_a, t_list *list_b)
 		node_a->prev = list_a->head;
 		node_a->next = list_a->head->next;
 		list_a->head->next = node_a;
+
+		current_a = list_a->head;
+		while (current_a->next != NULL)
+		{
+			current_a = current_a->next;
+			if (current_a->next == NULL)
+				list_a->tail = current_a;
+		}
 	}
 	if (list_b != NULL && list_b->head != NULL && \
 	list_b->head->next != NULL)
@@ -67,5 +95,13 @@ void	ss(t_list *list_a, t_list *list_b)
 		node_b->prev = list_b->head;
 		node_b->next = list_b->head->next;
 		list_b->head->next = node_b;
+
+		current_b = list_b->head;
+		while (current_b->next != NULL)
+		{
+			current_b = current_b->next;
+			if (current_b->next == NULL)
+				list_b->tail = current_b;
+		}
 	}
 }
