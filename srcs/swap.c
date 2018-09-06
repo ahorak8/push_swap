db0d625a1ae22357b093f816d5cb749182682d01
aleksiel HORAK <ahorak@c2r4s1.wethinkcode.co.za>
2018-09-06 14:38:23 +0200

    Cleaner code for swap

--- srcs/swap.c
+++ srcs/swap.c
@@ -12,96 +12,43 @@
 
 #include "../includes/push_swap.h"
 
-void	sa(t_list *list_a)
+void	swap(t_list *list)
 {
 	t_node	*node;
 	t_node	*current;
 
-	if (list_a != NULL && list_a->head != NULL && list_a->head->next != NULL)
-	{
-		node = list_a->head;
-		list_a->head = node->next;
-		list_a->head->prev = NULL;
-		node->prev = list_a->head;
-		node->next = list_a->head->next;
-		list_a->head->next = node;
+	node = list->head;
+	list->head = node->next;
+	list->head->prev = NULL;
+	node->prev = list->head;
+	node->next = list->head->next;
+	list->head->next = node;
 
-		current = list_a->head;
-		while (current->next != NULL)
-		{
-			current = current->next;
-			if (current->next == NULL)
-				list_a->tail = current;
-		}
+	current = list->head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+		if (current->next == NULL)
+			list->tail = current;
 	}
+}
+
+void	sa(t_list *list_a)
+{
+	if (list_a != NULL && list_a->head != NULL && list_a->head->next != NULL)
+		swap(list_a);
 }
 
 void	sb(t_list *list_b)
 {
-	t_node	*node;
-	t_node	*current;
-
-	if (list_b != NULL && list_b->head != NULL && \
-	list_b->head->next != NULL)
-	{
-		node = list_b->head;
-		list_b->head = node->next;
-		list_b->head->prev = NULL;
-		node->prev = list_b->head;
-		node->next = list_b->head->next;
-		list_b->head->next = node;
-
-		current = list_b->head;
-		while (current->next != NULL)
-		{
-			current = current->next;
-			if (current->next == NULL)
-				list_b->tail = current;
-		}
-	}
+	if (list_b != NULL && list_b->head != NULL && list_b->head->next != NULL)
+		swap(list_b);
 }
 
 void	ss(t_list *list_a, t_list *list_b)
 {
-	t_node	*node_a;
-	t_node	*node_b;
-	t_node	*current_a;
-	t_node	*current_b;
-
-	if (list_a != NULL && list_a->head != NULL && \
-	list_a->head->next != NULL)
-	{
-		node_a = list_a->head;
-		list_a->head = node_a->next;
-		list_a->head->prev = NULL;
-		node_a->prev = list_a->head;
-		node_a->next = list_a->head->next;
-		list_a->head->next = node_a;
-
-		current_a = list_a->head;
-		while (current_a->next != NULL)
-		{
-			current_a = current_a->next;
-			if (current_a->next == NULL)
-				list_a->tail = current_a;
-		}
-	}
-	if (list_b != NULL && list_b->head != NULL && \
-	list_b->head->next != NULL)
-	{
-		node_b = list_b->head;
-		list_b->head = node_b->next;
-		list_b->head->prev = NULL;
-		node_b->prev = list_b->head;
-		node_b->next = list_b->head->next;
-		list_b->head->next = node_b;
-
-		current_b = list_b->head;
-		while (current_b->next != NULL)
-		{
-			current_b = current_b->next;
-			if (current_b->next == NULL)
-				list_b->tail = current_b;
-		}
-	}
+	if (list_a != NULL && list_a->head != NULL && list_a->head->next != NULL)
+		swap(list_a);
+	if (list_b != NULL && list_b->head != NULL && list_b->head->next != NULL)
+		swap(list_b);
 }
